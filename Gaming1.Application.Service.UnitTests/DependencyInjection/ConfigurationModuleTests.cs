14a75b989ed6ffc703345429d0061591d7b4d018
Pedro Maroto <pedrom@cubictelecom.com>
2023-04-06 16:19:27 +0200

    Add missing unit test for resolving dependencies injections in the Service project.

--- Gaming1.Application.Service.UnitTests/DependencyInjection/ConfigurationModuleTests.cs
+++ Gaming1.Application.Service.UnitTests/DependencyInjection/ConfigurationModuleTests.cs
@@ -3,6 +3,7 @@ using Autofac.Builder;
 using FluentAssertions;
 using Gaming1.Application.Service.DependencyInjection;
 using Gaming1.Application.Service.Resolvers;
+using Gaming1.Application.Service.Services;
 using Xunit;
 
 namespace Gaming1.Application.Service.UnitTests.DependencyInjection
@@ -25,6 +26,54 @@ namespace Gaming1.Application.Service.UnitTests.DependencyInjection
             // Assert
             result.Should().BeAssignableTo<IGameResolver>();
             result.Should().BeOfType<WinnerResolver>();
+        }
+
+        [Fact]
+        public void ResolveISecretNumberGenerator_Return_SecretNumberGenerator()
+        {
+            // Arrange
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterHandlers();
+            _container = containerBuilder.Build(ContainerBuildOptions.IgnoreStartableComponents);
+
+            // Act
+            var result = _container.Resolve<ISecretNumberGenerator>();
+
+            // Assert
+            result.Should().BeAssignableTo<ISecretNumberGenerator>();
+            result.Should().BeOfType<SecretNumberGenerator>();
+        }
+
+        [Fact]
+        public void ResolveIPlayerGenerator_Return_PlayerGenerator()
+        {
+            // Arrange
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterHandlers();
+            _container = containerBuilder.Build(ContainerBuildOptions.IgnoreStartableComponents);
+
+            // Act
+            var result = _container.Resolve<IPlayerGenerator>();
+
+            // Assert
+            result.Should().BeAssignableTo<IPlayerGenerator>();
+            result.Should().BeOfType<PlayerGenerator>();
+        }
+
+        [Fact]
+        public void ResolveIGameFactory_Return_GameFactory()
+        {
+            // Arrange
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterHandlers();
+            _container = containerBuilder.Build(ContainerBuildOptions.IgnoreStartableComponents);
+
+            // Act
+            var result = _container.Resolve<IGameFactory>();
+
+            // Assert
+            result.Should().BeAssignableTo<IGameFactory>();
+            result.Should().BeOfType<GameFactory>();
         }
     }
 }
